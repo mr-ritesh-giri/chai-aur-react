b8c7c4bd20e6ae28f51e4bf19931d4e51e7043d0
Ritesh Giri <rrgiri032@gmail.com>
2024-07-03 12:06:22 +0530

    Count Done

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// function MyApp() {
+//   return (
+//     <>
+//       <h1>Chai aur react</h1>
+//       <p>Chai aur para</p>
+//     </>
+//   );
+// }
+
+// const ReactElement = {
+//   type: "a",
+//   props: { href: "https://www.google.com/", target: "_blank" },
+//   children: "Click me to visit google",
+// };
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
+// );
+
+const Username = " Chai aur React";
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Visit here",
+  Username
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
--- Custom-React/customReact.jsx
+++ Custom-React/customReact.jsx
@@ -1,26 +1,24 @@
-function customRender(reactElement, root) {
-  // const domElement = document.createElement(reactElement.type);
-  // domElement.innerHTML = reactElement.children;
-  // domElement.setAttribute("href", reactElement.props.href);
-  // domElement.setAttribute("target", reactElement.props.target);
-
-  // root.appendChild(domElement);
-
+function customRender(reactElement, mainContainer) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
+
   for (const prop in reactElement.props) {
-    if (prop == "children") continue;
+    if (prop === "children") continue;
     domElement.setAttribute(prop, reactElement.props[prop]);
   }
-  root.appendChild(domElement);
+  mainContainer.appendChild(domElement);
 }
 
 const reactElement = {
   type: "a",
   props: { href: "https://www.google.com/", target: "_blank" },
-  children: "Click me to visit google.",
+  children: "Click me to visit google",
 };
 
-const root = document.getElementById("root");
+const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
 
-customRender(reactElement, root);
+//   domElement.setAttribute("href", reactElement.props.href);
+//   domElement.setAttribute("target", reactElement.props.target);
+
+//   mainContainer.appendChild(domElement);
