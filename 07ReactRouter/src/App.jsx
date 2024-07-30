893767efccc15fcdbb1fa94841412b4165d66499
Ritesh Giri <rrgiri032@gmail.com>
2024-07-30 12:22:09 +0530

    Name Changing

---
+++ 07ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Home />
+    </>
+  );
+}
+
+export default App;
