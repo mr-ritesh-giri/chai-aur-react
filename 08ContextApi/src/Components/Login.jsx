d750f858ed577e835d4bc1ca7e5dab127b870d50
Ritesh Giri <rrgiri032@gmail.com>
2024-09-05 11:33:17 +0530

    Context Api

--- 08ContextApi/src/Components/Login.jsx
+++ 08ContextApi/src/Components/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
       />
       {""}
       <input
-        type="text"
+        type="password"
         placeholder="Password"
         className="rounded-md p-2 text-black"
         value={password}
--- 08ContextApi/src/Components/Profile.jsx
+++ 08ContextApi/src/Components/Profile.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import UserContext from "../Context/userContext";
 
 function Profile() {
   const { user } = useContext(UserContext);
-  
+
   if (!user) return <div>Please Login First</div>;
 
-  return <div>Welcome {user.username}</div>;
+  return (
+    <div>
+      Welcome {user.username} & your password is {user.password}.
+    </div>
+  );
 }
 
 export default Profile;
