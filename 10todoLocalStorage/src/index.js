7bdeb2229fc4fed457d1e22656ce94b4ac0b26e7
Ritesh Giri <rrgiri032@gmail.com>
2024-09-05 22:06:34 +0530

    Todo with localstorage done 50% now local storage will be added here from now on.

---
+++ 10todoLocalStorage/src/index.js
@@ -0,0 +1,3 @@
+export { default as TodoForm } from "./Components/TodoForm";
+export { default as TodoItem } from "./Components/TodoItem";
+export { TodoContext, TodoProvider, useTodo } from "./Context/TodoContext";
