86400d0524e94ff438cbbbb222f6f29734cb6aff
Ritesh Giri <rrgiri032@gmail.com>
2024-09-05 12:50:54 +0530

    Context Api

--- 08ContextApi/src/Context/UserContextProvider.jsx
+++ 08ContextApi/src/Context/UserContextProvider.jsx
@@ -3,9 +3,13 @@ import UserContext from "./userContext";
 
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
+  // const value = { user, setUser };
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ user, setUser }}
+      // value={value} It can also be done.
+    >
       {children}
     </UserContext.Provider>
   );
