c24a95e12d9f056cfe764a13c5366bd77e20c03e
Ritesh Giri <rrgiri032@gmail.com>
2024-09-07 20:28:22 +0530

    Redux Toolkit Begins

---
+++ 11reduxToolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-black">
+      <h1 className="text-3xl font-bold text-white text-center">
+        React Redux Toolkit!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
